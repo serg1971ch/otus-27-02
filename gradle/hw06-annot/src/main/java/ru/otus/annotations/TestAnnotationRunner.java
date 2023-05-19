97f1a11fb23833f588185d19f16f71ca4f496e87
Сергей <34056892+serg1971ch@users.noreply.github.com>
2023-05-19 08:06:01 +0300

    Домашнее задание №3,исправление замечаний

--- gradle/hw06-annot/src/main/java/ru/otus/annotations/TestAnnotationRunner.java
+++ gradle/hw06-annot/src/main/java/ru/otus/annotations/TestAnnotationRunner.java
@@ -7,12 +7,12 @@ import ru.otus.annotations.annotation.Before;
 import ru.otus.annotations.annotation.Test;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
 import static ru.otus.reflection.ReflectionHelper.callMethod;
-import static ru.otus.reflection.ReflectionHelper.instantiate;
 
 
 public class TestAnnotationRunner {
@@ -37,10 +37,8 @@ public class TestAnnotationRunner {
 
     public void runTests() {
         logger.info("-------Starting tests runner for {}", testClassName);
-
-        Class<?> clazz;
+        final Class<?> clazz  = getClassForName(testClassName);
         try {
-            clazz = Class.forName(testClassName);
             for (Method method : clazz.getDeclaredMethods()) {
                 for (Annotation annotation : method.getAnnotations()) {
                     String name = annotation.annotationType().getName();
@@ -57,26 +55,32 @@ public class TestAnnotationRunner {
             logger.info("@Before methods: {}", beforeMethods.stream().map(Method::getName).toList());
             logger.info("@Test methods: {}\n", testMethods.stream().map(Method::getName).toList());
             logger.info("@After methods: {}", afterMethods.stream().map(Method::getName).toList());
-            Object testInstance = instantiate(clazz);
 
             for (Method testMethod : testMethods) {
-                performedTest += runTest(testInstance, beforeMethods, testMethod, afterMethods);
+                runTest(clazz, beforeMethods, testMethod, afterMethods);
             }
 
-        } catch (ClassNotFoundException | RuntimeException e) {
+        } catch (RuntimeException e) {
             logger.error("Error while test class or instance initialisation: ", e);
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
         }
         printResult();
     }
 
 
-    private int runTest(Object instance, List<Method> beforeMethods, Method testMethod, List<Method> afterMethod) {
-        int executed = 0;
+    private <T> void runTest(final Class<T> instance, List<Method> beforeMethods, Method testMethod, List<Method> afterMethod) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        final T testObject = instance.getDeclaredConstructor().newInstance();
         try {
             beforeMethods.forEach(method -> callMethod(instance, method.getName()));
             callMethod(instance, testMethod.getName());
-
-            executed++;
+            performedTest++;
         } catch (RuntimeException e) {
             logger.error("Error while setUp or run test", e);
         }
@@ -86,13 +90,20 @@ public class TestAnnotationRunner {
         } catch (RuntimeException e) {
             logger.error("Error while teardown", e);
         }
-        return executed;
     }
 
     private void printResult() {
         int testsCount = testMethods.size();
         logger.info("\n--------------\nTESTS RESULTS:\n   TESTS: {}\n    GOOD: {}\n     BAD: {}\n", testsCount,
                 performedTest, testsCount - performedTest);
+    }
+
+    private Class<?> getClassForName(final String className) {
+        try {
+            return Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            return null;
+        }
     }
 }
 
