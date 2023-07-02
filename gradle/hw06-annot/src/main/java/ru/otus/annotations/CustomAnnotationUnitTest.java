aa1138d7e2658f23d6bf0898858a9ef16e412d52
serg1971ch <serg1971ch@yandex.ru>
2023-07-02 09:53:27 +0300

    This is a homework about OOP. hw12-oop

--- gradle/hw06-annot/src/main/java/ru/otus/annotations/CustomAnnotationUnitTest.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.annotations;
-
-public class CustomAnnotationUnitTest {
-    public static void main(String[] args) {
-        new TestAnnotationRunner("ru.otus.annotations.AnnotationsTest").runTests();
-    }
-}
